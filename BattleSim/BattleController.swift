614b74ecc0872b5a872987be525929d9aa69ee73
Waluigi <No>
2026-04-09 11:56:49 -0700

    Battle Menu Made

---
+++ BattleSim/BattleController.swift
@@ -0,0 +1,8 @@
+//
+//  BattleController.swift
+//  BattleSim
+//
+//  Created by Quintin Dennison on 2026-04-09.
+//
+
+import Foundation
---
+++ BattleSim/Views/JournalView.swift
@@ -0,0 +1,8 @@
+//
+//  JournalView.swift
+//  BattleSim
+//
+//  Created by Quintin Dennison on 2026-04-09.
+//
+
+import Foundation
