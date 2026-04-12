a35c4dae612f03f26ed2deb72527db880cb37dbf
ThatWaluigi <ThatWaluigi@gmail.com>
2026-04-11 17:54:01 -0700

    added default DB

--- BattleSim/Views/ContentView.swift
+++ BattleSim/Views/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     @State var screen: Screen = .village
-
     @State var player = PlayerObj(money: 0, health: 15, maxHealth: 20, maxWeapons: 1, weapons: [])
+
+    @Enviroment(\.ModelContext) private var context
+    
+    enum Screen {
+        case village, battle, journal
+    }
     
     var body: some View {
         switch screen {
@@ -18,10 +23,10 @@ struct ContentView: View {
             case .battle: BattleView(player: $player, Return: { screen = .village })
             case .journal: JournalView(Return: { screen = .village })
         }
-    }
 
-    enum Screen {
-        case village, battle, journal
+        .task {
+            DataDefaults.seedDatabase(context: context)
+        }
     }
 }
 
